5f009d1bb1035a6f16ad7b1d5cbdadd90968b196
unknown <s.moh_ali@outlook.com>
2024-09-22 16:20:22 -0400

    third commit

---
+++ Source Files/GroceryProduct.java
@@ -0,0 +1,11 @@
+// grocery product class for all grocery products; setters and getters based on file read
+public abstract class GroceryProduct {
+    protected double price;
+
+    public abstract void setPrice(double price);
+
+    public double getPrice() {
+        return price;
+    }
+}
+
---
+++ Source Files/GroceryProductFactory.java
@@ -0,0 +1,4 @@
+//interface for groceryproduct factory to follow, allows for method makeProduct() for any given grocery item
+public interface GroceryProductFactory {
+    GroceryProduct makeProduct();
+}
