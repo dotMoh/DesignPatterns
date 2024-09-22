e743d931e1b1957a4fb086acce398400984bdbde
Mohammad Ali <s.moh_ali@outlook.com>
2024-09-22 15:51:34 -0400

    Delete AppleFactory.java

--- AppleFactory.java
+++
@@ -1,7 +0,0 @@
-//creates new apple based on groceryproductfactory interface
-public class AppleFactory implements GroceryProductFactory {
-    @Override
-    public GroceryProduct makeProduct() {
-        return new Apple();
-    }
-}
