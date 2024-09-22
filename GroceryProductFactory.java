798b0fd4f2d452356fd0cd96164b70c00d7c6bf8
Mohammad Ali <s.moh_ali@outlook.com>
2024-09-22 15:51:24 -0400

    Delete GroceryProductFactory.java

--- GroceryProductFactory.java
+++
@@ -1,4 +0,0 @@
-//interface for groceryproduct factory to follow, allows for method makeProduct() for any given grocery item
-public interface GroceryProductFactory {
-    GroceryProduct makeProduct();
-}
