5bc51ef5b8495f8eff908243d462db709310ce7a
Mohammad Ali <s.moh_ali@outlook.com>
2024-09-22 15:51:11 -0400

    Delete OrangeFactory.java

--- OrangeFactory.java
+++
@@ -1,7 +0,0 @@
-//creates new orange based on groceryproductfactory interface
-public class OrangeFactory implements GroceryProductFactory {
-    @Override
-    public GroceryProduct makeProduct() {
-        return new Orange();
-    }
-}
