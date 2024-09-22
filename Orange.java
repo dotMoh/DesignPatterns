25804c7e9503604e6a4c8becca42e26c71627097
Mohammad Ali <s.moh_ali@outlook.com>
2024-09-22 15:51:15 -0400

    Delete Orange.java

--- Orange.java
+++
@@ -1,8 +0,0 @@
-//this class takes methods from GroceryProduct in order to setPrice on orange
-public class Orange extends GroceryProduct {
-    @Override
-    public void setPrice(double price) {
-        this.price = price;
-        System.out.printf("Orange price read from file $%.2f%n", this.price);
-    }
-}
