8f72866e8b75a9e449d21bfdbc32f10ce7b1bcba
Mohammad Ali <s.moh_ali@outlook.com>
2024-09-22 15:51:39 -0400

    Delete Apple.java

--- Apple.java
+++
@@ -1,8 +0,0 @@
-//this class takes methods from GroceryProduct in order to setPrice on apple
-public class Apple extends GroceryProduct {
-    @Override
-    public void setPrice(double price) {
-        this.price = price;
-        System.out.printf("Apple price read from file $%.2f%n", this.price);
-    }
-}
